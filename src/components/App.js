68c54170b02cd2f5fea95842ed1c6a4174065f8c
sumit7057416306 <120789691+sumit7057416306@users.noreply.github.com>
2023-04-02 19:16:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,53 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
 
-const App = () => {
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+
+const Autocomplete = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const filteredSuggestions = fruits.filter(
+      (fruit) =>
+        fruit.toLowerCase().indexOf(inputValue.toLowerCase()) !== -1
+    );
+    setSuggestions(filteredSuggestions);
+  }, [inputValue]);
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+     <div className="container">
+      
+      
+      <div className="autocomplete">
+      <div className="title">
+        <h1>Search item</h1>
+        
 
-export default App
+      </div>
+      <br />
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleChange}
+        placeholder="Search for a fruit"
+      />
+      <br />
+      <ul>
+        {suggestions.map((suggestion) => (
+          <li key={suggestion}>{suggestion}</li>
+        ))}
+      </ul>
+    </div>
+
+     </div>
+    
+  );
+};
+
+export default Autocomplete;
